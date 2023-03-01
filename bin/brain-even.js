33981a96b9ce3361be56a11a1c00d0f0460641df
Iana Sibrina <kidacole@gmail.com>
2023-03-01 22:14:20 +0300

    5th step of project

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,29 +1,26 @@
 #!/usr/bin/env node
 
-
 import readlineSync from 'readline-sync';
 import userName from '../src/cli.js';
 
 console.log('Answer "yes" if the number is even, otherwise answer "no".');
 const numderCheck = () => {
-    for (let i = 0; i < 3; i += 1) {
-        let getRandomNumber = Math.ceil(Math.random() * 100);
-        let playerAnswer = readlineSync.question('Question: ' + getRandomNumber);
+    let i = 1;
+    while (i <= 3) {
+        let randomNum = Math.ceil(Math.random() * 100);
+        let playerAnswer = readlineSync.question('Question: ' + randomNum);
         console.log('Your answer: ' + playerAnswer);
 
-        if (playerAnswer !== 'yes' && playerAnswer !== 'no') {
+        if (randomNum % 2 === 0 && playerAnswer === 'yes' || randomNum % 2 !== 0 && playerAnswer === 'no') {
+            console.log('Correct!');
+            i +=1;
+            if (i > 3) {
+                console.log('Congratulations, ' + userName + '!');
+            }    
+        } else {
             console.log(playerAnswer + ' is wrong answer ;(. \nLet\'s try again, ' + userName + '!');
             break;
-        } else if (getRandomNumber % 2 === 0 && playerAnswer === 'yes' || getRandomNumber % 2 !== 0 && playerAnswer === 'no') {
-            console.log('Correct!');
-        } else if (getRandomNumber % 2 === 0 && playerAnswer === 'no') {
-            console.log('\'no\' is wrong answer ;(. Correct answer was \'yes\'. \nLet\'s try again, ' + userName + '!');
-            break;
-        } else if (getRandomNumber % 2 !== 0 && playerAnswer === 'yes') {
-            console.log('\'yes\' is wrong answer ;(. Correct answer was \'no\'. \nLet\'s try again, ' + userName + '!');
-            break;
         }  
-    }
-    console.log('Congratulations, ' + userName + '!');
+        }       
 };
 numderCheck();
