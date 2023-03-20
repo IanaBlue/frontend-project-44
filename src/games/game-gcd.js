cc6f95331177eca0a32e441ee78d72ab04388479
Iana Sibrina <kidacole@gmail.com>
2023-03-20 13:34:18 +0300

    fix gcd game

--- src/games/game-gcd.js
+++ src/games/game-gcd.js
@@ -1,20 +1,21 @@
 import gameProcess from '../index.js';
 
 const maxDivider = (num1, num2) => {
-    const result = [];
-    for (let i = 1; i !== (num1 + num2) / 2; i += 1) {
-      if (num1 % i === 0 && num2% i === 0) {
-      result.push(i)
+  while (num1 !== num2) {
+    if (num1 > num2) {
+      num1 -= num2;
+    } else {
+      num2 -= num1;
     }
-    }
-    return result[result.length - 1]
-  };
+  }
+  return (num1 || num2);
+};  
 
 const taskOfRound = 'Find the greatest common divisor of given numbers.';
 
 const gameRaund = () => {
-    const num1 = Math.ceil(Math.random() * 50);
-    const num2 = Math.ceil(Math.random() * 50);
+    const num1 = Math.ceil(Math.random() * 20);
+    const num2 = Math.ceil(Math.random() * 20);
 
     const question = `${num1} ${num2}`;
     const correctAnswer = String(maxDivider(num1, num2));
