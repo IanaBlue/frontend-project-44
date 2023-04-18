888141feaa12e7c8a7ae95899abb5c2c36496b6f
Iana Sibrina <kidacole@gmail.com>
2023-04-18 16:22:22 +0300

    fix progression-game

--- src/games/game-progression.js
+++ src/games/game-progression.js
@@ -1,9 +1,8 @@
 import gameProcess from '../index.js';
 
-const ramdomNum10 = Math.ceil(Math.random() * 10);
-const ramdomNum50 = Math.ceil(Math.random() * 50);
-
 const progressionNumbers = () => {
+    const ramdomNum10 = Math.ceil(Math.random() * 10);
+    const ramdomNum50 = Math.ceil(Math.random() * 50);
     const result = [];
     for (let i = ramdomNum50; result.length != 10; i += ramdomNum10) {
         result.push(i);
@@ -15,7 +14,7 @@ const taskOfRound = 'What number is missing in the progression?';
 
 const gameRaund = () => {
     let currentProgressionNumbers = progressionNumbers();
-    let removed = currentProgressionNumbers.splice(ramdomNum10 - 1, 1, '..');
+    let removed = currentProgressionNumbers.splice((Math.random() * 10) - 1, 1, '..');
     
     const question = currentProgressionNumbers.toString();
     const correctAnswer = removed.toString();
