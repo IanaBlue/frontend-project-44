8762c03418e4733b1caff1b58e73ba8e53090094
Iana Sibrina <kidacole@gmail.com>
2023-04-23 22:40:55 +0300

    fix eslint error

--- src/games/game-progression.js
+++ src/games/game-progression.js
@@ -4,7 +4,7 @@ const progressionNumbers = () => {
   const ramdomNum10 = Math.ceil(Math.random() * 10);
   const ramdomNum50 = Math.ceil(Math.random() * 50);
   const result = [];
-  for (let i = ramdomNum50; result.length != 10; i += ramdomNum10) {
+  for (let i = ramdomNum50; result.length !== 10; i += ramdomNum10) {
     result.push(i);
   }
   return result;
