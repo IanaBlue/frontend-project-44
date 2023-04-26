1d24264cd8ae09b478da8f5da8a0b282213b74c2
Iana Sibrina <kidacole@gmail.com>
2023-04-26 16:39:55 +0300

    fix progrettion game

--- src/games/progression.js
+++ src/games/progression.js
@@ -1,7 +1,7 @@
 import startGameProcess from '../index.js';
 import generateRandomNum from '../helpers.js';
 
-const progressionNumbers = (startNum, increment, length) => {
+const makeProgressionNumbers = (startNum, increment, length) => {
   const result = [];
   for (let i = 0; i < length; i += 1) {
     result.push(startNum + i * increment);
@@ -16,8 +16,8 @@ const generateRound = () => {
   const increment = generateRandomNum(1, 10);
   const length = generateRandomNum(5, 10);
 
-  const currentProgressionNumbers = progressionNumbers(startNum, increment, length);
-  const removed = currentProgressionNumbers.splice((Math.random() * 10) - 1, 1, '..');
+  const currentProgressionNumbers = makeProgressionNumbers(startNum, increment, length);
+  const removed = currentProgressionNumbers.splice((Math.random() * currentProgressionNumbers.length) - 1, 1, '..');
 
   const question = currentProgressionNumbers.join(' ');
   const answer = removed.toString();
